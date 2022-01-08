d7782d98faf23944eb20f5b2c03388886d6826dd
zhibek <zhibek.ksmva@gmail.com>
2022-01-08 13:12:09 +0600

    model 'Guest' was registered in admin.py and work on templates and views.py was started

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from webapp.models import Guest
+# Register your models here.
+
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'text', 'creation_date', 'edition_date', 'status']
+    list_filter = ['status']
+    search_fields = ['name', 'email']
+    fields = ['name', 'email', 'text', 'creation_date', 'edition_date', 'status']
+    readonly_fields = ['creation_date', 'edition_date']
+
 
 # Register your models here.
+admin.site.register(Guest, GuestAdmin)
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from webapp.models import Guest, status_choices
 
 # Create your views here.
+
+
+def index(request):
+    guests = Guest.objects.order_by("name")
+    return render(request, 'index.html', {'guests': guests})
+
+
